5f339d1a29def3f55f4a35fb33ca9cb29262f740
Jesús Antonio García Valadez <jesus.garciav@me.com>
2020-06-15 23:38:10 -0500

    feat: change in implementation of delete method and mongo uri

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const Pets = require('./models/Pets')
 
-const { username, password, db } = functions.config
-mongoose.connect(`mongodb+srv://${username}:${password}@vet-api-a7qrs.gcp.mongodb.net/${db}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
+const { username, password, db } = functions.config().mongo
+mongoose.connect(`mongodb+srv://${username}:${password}@vet-api-iloft.gcp.mongodb.net/${db}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const createServer = () => {
     app.use(cors({ origin: true }))
@@ -19,13 +19,13 @@ const createServer = () => {
     app.post('/pets', async (req, res) => {
         const { body } = req
 
-        const pet = new Pets(body)
+        const pet = new Pets(JSON.parse(body))
         await pet.save()
 
-        res.sendStatus(204)
+        res.send({_id: pet.id})
     })
 
-    app.get('/pets/:id', async (req, res) => {
+    app.delete('/pets/:id/discharge', async (req, res) => {
         const { id } = req.params
 
         await Pets.deleteOne({ _id: id }).exec()
